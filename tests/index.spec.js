f9ce4c525dea65e0e44b84874fc0c34706b090c8
Dara Carolan <dara.carolan@accenture.com>
2018-04-03 10:02:49 +0100

    update packages, added link to favicon

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ApiPage from "../src/components/ApiPage/ApiPage.js";
 import { expect } from "chai";
-import { shallow } from "enzyme";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
 
 describe("HomePage tag test", () => {
   const wrapper = shallow(<ApiPage />);
